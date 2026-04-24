8ef14c67aa60a21cf66f256c84770cf96d57bb5f
Federico Martinez <fedemtzesc@hotmail.com>
2026-04-23 18:35:20 -0600

    Se cambio contenido por llamadas a los verbos HTML para pruebas nueva Fase III

--- src/main/java/com/fdxsoft/controllers/TestAuthController.java
+++ src/main/java/com/fdxsoft/controllers/TestAuthController.java
@@ -1,6 +1,10 @@
 package com.fdxsoft.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,18 +12,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class TestAuthController {
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello World!";
+	@GetMapping("/get")
+	public String helloGet() {
+		return "Hello World - GET";
 	}
 	
-	@GetMapping("/hello-secured")
-	public String helloSecured() {
-		return "Hello World Secured!";
+	@PostMapping("/post")
+	public String helloPost() {
+		return "Hello World - POST";
 	}
 	
-	@GetMapping("/hello-secured2")
-	public String helloSecured2() {
-		return "Hello World Secured 2!";
+	@PutMapping("/put")
+	public String helloPut() {
+		return "Hello World - PUT";
 	}
+	
+	@PatchMapping("/patch")
+	public String helloPatch() {
+		return "Hello World - PATCH";
+	}
+	
+	@DeleteMapping("/delete")
+	public String helloDelete() {
+		return "Hello World - DELETE";
+	}
+	
 }
